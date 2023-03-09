57bf08cd2b03600bbe414c30dd28aab1e6dc0aea
zinb9419 <zinb9419@gmail.com>
2023-03-09 20:32:06 +0100

    cpp06

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,13 +6,13 @@
 /*   By: ziloughm <ziloughm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 16:44:17 by ziloughm          #+#    #+#             */
-/*   Updated: 2023/03/02 14:50:35 by ziloughm         ###   ########.fr       */
+/*   Updated: 2023/03/07 22:08:05 by ziloughm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cast.hpp"
 
-int main(int ac, char **av)
+int main(void)
 {
     if (ac != 2)
         std::cout << "Nombre inavlid de paramètres" << std::endl;
